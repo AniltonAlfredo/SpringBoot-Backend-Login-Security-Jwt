bff93299c63831f5492e414a4a15db8075f9cd23
AniltonAlfredo <aniltonengenheiro@gmail.com>
2024-10-04 10:56:34 +0100

    projecto completo

---
+++ src/main/java/com/example/Xproject/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.Xproject.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.Xproject.models.User;
+import com.example.Xproject.repository.UserRepository;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+
+}
